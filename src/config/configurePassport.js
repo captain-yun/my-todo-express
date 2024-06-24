fdda229c921825f0e447e511935f5701a1408577
DESKTOP-E20NQFR\user <jslovejs182@gmail.com>
2024-06-25 00:52:47 +0900

    jwt전략 소문자로 변경

--- src/config/configurePassport.js
+++ src/config/configurePassport.js
@@ -1,7 +1,7 @@
-import passport from 'passport';
-import localStrategy from '../strategies/localStrategy.js';
-import jwtStrategy from '../strategies/jwtStrategy.js';
-import User from '../models/User.js';
+import passport from "passport";
+import localStrategy from "../strategies/localStrategy.js";
+import jwtStrategy from "../strategies/jwtStrategy.js";
+import User from "../models/User.js";
 
 const configurePassport = () => {
   passport.use(localStrategy);
